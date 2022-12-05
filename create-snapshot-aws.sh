5c96bdc34272e40b40df3b599c87f4e2cbc1bcdb
Michael Moreira <michaelmoreiramelo@gmail.com>
2022-12-05 14:52:28 +0000

    Update create-snapshot-aws.sh

--- create-snapshot-aws.sh
+++ create-snapshot-aws.sh
@@ -1,5 +1,5 @@
 #name instance
 aws ec2 create-snapshots \
-    --instance-specification InstanceId=i-0c2c8ed92c956261g \ #id instance
+    --instance-specification InstanceId=i-0c2c8ed92c956261g \  #id instance
     --copy-tags-from-source volume \
-    --description "name-instance_full:03112022-byawscli" #comment instance 
+    --description "name-instance_full:03112022-byawscli"       #comment instance 
